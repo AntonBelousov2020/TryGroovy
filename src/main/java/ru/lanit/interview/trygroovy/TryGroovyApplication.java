305308ec08f92cd30bc1afa7122f81f03792a46c
abelousov <abelousov@lanit.ru>
2026-06-12 22:25:50 +0400

    Удалил все остатки war наследия

--- src/main/java/ru/lanit/interview/trygroovy/TryGroovyApplication.java
+++ src/main/java/ru/lanit/interview/trygroovy/TryGroovyApplication.java
@@ -2,19 +2,12 @@ package ru.lanit.interview.trygroovy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TryGroovyApplication extends SpringBootServletInitializer {
+public class TryGroovyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TryGroovyApplication.class, args);
 	}
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(TryGroovyApplication.class);
-    }
 
 }
